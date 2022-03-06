7c8af595dd57c1922386ae5e36215cf4f025623d
No Name <dev.haxuan@gmail.com>
2022-03-06 23:51:48 +0700

    boolean js

---
+++ boolean/index.js
@@ -0,0 +1,18 @@
+/**
+ * boolean == false
+ * 0
+ * false
+ * ''
+ * ""
+ * undefined
+ * NaN
+ * null
+ * 
+ * Ngược lại các giá trị trên là true
+ */
+let isChecked = '';
+if(isChecked){
+    console.log('Success!'); 
+}else{
+    console.log('Failed!');
+};
