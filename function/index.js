7c34c1be9c3d2a34b91eb9ef00c307c73bf2a62f
No Name <dev.haxuan@gmail.com>
2022-03-08 02:11:33 +0700

    function

---
+++ function/index.js
@@ -0,0 +1,15 @@
+/**
+ * Các loại function
+ * 1. Declaration function
+ * 2. expression function
+ * 3. arrow function
+ */
+
+// ví dụ 1:
+function logWrite(){};
+
+// ví dụ 2:
+const a = function() {}
+
+// ví dụ 3:
+const log = () => {}
