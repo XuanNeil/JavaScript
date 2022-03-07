b148dfa5d53788f323936370b5deb29ed195c3dd
Ngoclt <ngoclt@reactplus.com>
2022-03-07 23:12:37 +0700

    array methods

---
+++ array/index.js
@@ -0,0 +1,22 @@
+/**
+ * Làm việc với Array
+ * References:
+ * -
+ * -
+ */
+
+/**
+ * 1. toString
+ * 2. join
+ * 3. pop
+ * 4. push
+ * 5. shift
+ * 6. unShift
+ * 7. splice
+ * 8. slide
+ * 9. concat
+ * 10. includes
+ */
+
+const list = ['JS', 'HTML', 'Java', 'Python'];
+
